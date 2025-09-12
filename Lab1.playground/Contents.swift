4434ec2b39ca628143e9d4ffca9d5715120555ca
Aibar <a_shakeyev@kbtu.kz>
2025-09-12 23:59:12 +0500

    lab1 upload

---
+++ Lab1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+let myName = "Aibar"
+let mySurname = "Shakeyev"
+let myAge: Int = 20
+let myBirthYear: Int = 2005
+let isStudent: Bool = true
+let myHeight: Double = 1.9
+let myWeight: Double = 70.5
+let myCity: String = "Almaty"
+
+let currenYear: Int = 2025
+print(currenYear - myBirthYear)
+
+let myFavHobby: String = "Cycling"
+let numberOfHobbies: Int = 5 // Maybe? IDK, not counted
+let myFavNumber: Int = 57 // Not sure, could be 7
+let isHobbyCreative: Bool = true // one of them is def true
+
+let myLiveStory = """
+    My name is \(myName) \(mySurname). I'm \(myAge), born in \(myBirthYear). I live in \(myCity). I'm \(myHeight) meters tall and weigh \(myWeight) kilograms. I like \(myFavHobby). My favorite number is \(myFavNumber). I have \(numberOfHobbies) hobbies. My favorite number is \(myFavNumber). I'm a student at KBTU.
+    """
+
+print(myLiveStory)
