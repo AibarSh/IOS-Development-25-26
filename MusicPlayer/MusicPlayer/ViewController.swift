28ac916b9e67fab0de132087014d37d2498d7d9f
Aibar <a_shakeyev@kbtu.kz>
2025-11-04 18:37:55 +0500

    lab6 upload

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Macbook Air on 30.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
